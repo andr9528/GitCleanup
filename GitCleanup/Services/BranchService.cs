01e55e19174ead82f64ee3e2fd46e387343bc35d
andr9528 <andre@steenhoff.dk>
2022-10-25 15:38:06 +0200

    Sort Patterns and start on finding branches with uncommited changes.

--- GitCleanup/Services/BranchService.cs
+++ GitCleanup/Services/BranchService.cs
@@ -9,27 +9,35 @@ namespace GitCleanup.Services
         private const string GIT_GET_ALL_REMOTE_BRANCHES =
             @"git for-each-ref --sort=creatordate --format '%(refname) %(creatordate)' refs/remotes/origin";
 
-        private readonly IEnumerable<(Area Area, Regex Pattern)> branchPatterns =
-            new List<(Area Area, Regex Pattern)>
-            {
-                (Area.CORE, new Regex(@"\/feat\/")),
-                (Area.GATEWAY_INEWS, new Regex(@"\/feat\/")),
-                (Area.BLUEPRINTS, new Regex(@"\/feat\/")),
-                (Area.BLUEPRINTS, new Regex(@"\/test\/")),
-                (Area.BLUEPRINTS, new Regex(@"\/chore\/")),
-                (Area.BLUEPRINTS, new Regex(@"\/fix\/")),
-                (Area.CORE, new Regex(@"\/feature\/")),
-                (Area.CORE, new Regex(@"\/fix\/")),
-                (Area.GATEWAY_INEWS, new Regex(@"\/fix\/")),
-                (Area.CORE, new Regex(@"\/contribute\/")),
-                (Area.CORE, new Regex(@"\/dist\/")),
-                (Area.CORE, new Regex(@"\/test\/")),
-            };
+        private const string GIT_GET_ALL_REMOTE_BRANCHES_WITH_UNMERGED = @"git branch --r --no-merged";
+
+        private readonly IEnumerable<(Area Area, Regex Pattern)> branchPatterns = new List<(Area Area, Regex Pattern)>
+        {
+            (Area.GATEWAY_INEWS, new Regex(@"\/feat\/")),
+            (Area.GATEWAY_INEWS, new Regex(@"\/fix\/")),
+            (Area.BLUEPRINTS, new Regex(@"\/feat\/")),
+            (Area.BLUEPRINTS, new Regex(@"\/test\/")),
+            (Area.BLUEPRINTS, new Regex(@"\/chore\/")),
+            (Area.BLUEPRINTS, new Regex(@"\/fix\/")),
+            (Area.CORE, new Regex(@"\/feat\/")),
+            (Area.CORE, new Regex(@"\/feature\/")),
+            (Area.CORE, new Regex(@"\/fix\/")),
+            (Area.CORE, new Regex(@"\/contribute\/")),
+            (Area.CORE, new Regex(@"\/dist\/")),
+            (Area.CORE, new Regex(@"\/test\/")),
+            (Area.CORE, new Regex(@"\/refactor\/")),
+        };
 
         private void BuildGetBranchesCommand(PowerShell shell, KeyValuePair<Area, string> area)
         {
             shell.AddScript($"cd {area.Value}");
             shell.AddScript($"{GIT_GET_ALL_REMOTE_BRANCHES}");
+        }
+
+        private void BuildGetUnmergedBranchesCommand(PowerShell shell, KeyValuePair<Area, string> area)
+        {
+            shell.AddScript($"cd {area.Value}");
+            shell.AddScript($"{GIT_GET_ALL_REMOTE_BRANCHES_WITH_UNMERGED}");
         }
 
         public void WriteBranches(Dictionary<Area, string> areas)
@@ -49,6 +57,20 @@ namespace GitCleanup.Services
                     FindMatchingPowershellLines(branches, branchPatterns.Where(x => x.Area == area.Key).ToList());
                 Console.WriteLine($"Total {area.Key} Branches Count to Delete: {deleteBranches.Count}");
                 //WritePowershellLines(deleteBranches, area);
+
+                double percentageRemoved = Math.Round((double) deleteBranches.Count / branches.Count * 100, 3);
+                Console.WriteLine($"Percentage {area.Key} Branches to be Delete: {percentageRemoved}%");
+
+                shell.Commands.Clear();
+                BuildGetUnmergedBranchesCommand(shell, area);
+
+                var unmergedBranches = RunPSScript(shell);
+                Console.WriteLine($"Total {area.Key} Branches with Unmerged changes Count: {unmergedBranches.Count}");
+
+                var deleteUnmergedBranches = deleteBranches.Where(x =>
+                    unmergedBranches.Any(y =>
+                        x.ImmediateBaseObject.ToString().Contains(y.ImmediateBaseObject.ToString()))).ToList();
+                Console.WriteLine($"Total {area.Key} Branches to be deleted with Unmerged changes Count: {deleteUnmergedBranches.Count}");
             }
         }
     }
--- GitCleanup/Services/TagService.cs
+++ GitCleanup/Services/TagService.cs
@@ -9,15 +9,14 @@ namespace GitCleanup.Services
         private const string GIT_GET_ALL_TAGS =
             "git for-each-ref --sort=creatordate --format '%(refname) %(creatordate)' refs/tags";
 
-        private readonly IEnumerable<(Area Area, Regex Pattern)> tagPatterns =
-            new List<(Area Area, Regex Pattern)>
-            {
-                (Area.CORE, new Regex(@"dist_blueprints_integration\d{8}_\d")),
-                (Area.BLUEPRINTS, new Regex(@"v\d{8}_")),
-                (Area.GATEWAY_INEWS, new Regex(@"v\d{8}_")),
-                (Area.CORE, new Regex(@"v\d{8}_")),
-                (Area.CORE, new Regex(@"v\d\.\d\d?\.\d_\d{8}_\d")),
-            };
+        private readonly IEnumerable<(Area Area, Regex Pattern)> tagPatterns = new List<(Area Area, Regex Pattern)>
+        {
+            (Area.BLUEPRINTS, new Regex(@"v\d{8}_")),
+            (Area.GATEWAY_INEWS, new Regex(@"v\d{8}_")),
+            (Area.CORE, new Regex(@"v\d{8}_")),
+            (Area.CORE, new Regex(@"dist_blueprints_integration\d{8}_\d")),
+            (Area.CORE, new Regex(@"v\d\.\d\d?\.\d_\d{8}_\d")),
+        };
 
         private void BuildGetTagsCommand(PowerShell shell, KeyValuePair<Area, string> area)
         {
@@ -41,6 +40,9 @@ namespace GitCleanup.Services
                 var deleteTags = FindMatchingPowershellLines(tags, tagPatterns.Where(x => x.Area == area.Key).ToList());
                 Console.WriteLine($"Total {area.Key} Tags Count to Delete: {deleteTags.Count}");
                 //WritePowershellLines(deleteTags, area);
+
+                double percentageRemoved = Math.Round((double) deleteTags.Count / tags.Count * 100, 3);
+                Console.WriteLine($"Percentage {area.Key} Tags to be Delete: {percentageRemoved}%");
             }
         }
     }
